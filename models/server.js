26925055c34dbfa1445e4115a83bca2c6641c2bd
samRgarcia <samuelrivas18@gmail.com>
2020-12-21 21:34:21 -0600

    first commit

--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketio= require('socket.io');
 const path = require('path');
+const cors = require('cors')
 
 const Sockets = require('./socket');
 
@@ -18,6 +19,9 @@ class Server {
 
     middlewares(){
        this.app.use(express.static(path.resolve(__dirname,'../public')));
+
+       //CORS
+       this.app.use(cors());
     }
 
     cofigurarSockets(){
